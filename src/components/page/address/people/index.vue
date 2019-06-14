657c026d724a2278aef13fff45d4a1c2fcd5cc8e
huipaodejitui <920210993@qq.com>
2019-06-14 23:06:29 +0800

    个人名片

--- src/components/page/address/people/index.vue
+++ src/components/page/address/people/index.vue
@@ -1,27 +1,51 @@
 <template>
   <CommonPopPage :op="opPage">
-    <div class="album">
-      <img src alt>
-      <div>
+    <div class="popple">
+      <div class="photo">
+        <img src alt>
+      </div>
+      <div class="card">
         <h3>{{this.$route.params.id.slice(1)}}</h3>
-        <div>
+        <div class="number">
           <span>微信号：</span>
           <span>2323</span>
         </div>
-        <div>
+        <div class="area">
           <span>地区：</span>
           <span>广东 深圳</span>
         </div>
       </div>
     </div>
+    <CommonUl :op="dataOp"/>
   </CommonPopPage>
 </template>
 
 <script>
 import CommonPopPage from "@/common/CommonPopPage";
+import CommonUl from "@/common/find/CommonUl";
 export default {
   data() {
     return {
+      dataOp: {
+        data: [
+          {
+            title: "设置备注和标签",
+            img: "friend",
+            route: "friend"
+          },
+          {
+            title: "朋友圈",
+            img: "scan",
+            route: "scan",
+            active: true
+          },
+          {
+            title: "更多信息",
+            img: "shake",
+            route: "shake"
+          }
+        ]
+      },
       opPage: {
         class: "find",
         header: true,
@@ -33,18 +57,30 @@ export default {
     };
   },
   components: {
-    CommonPopPage
+    CommonPopPage,
+    CommonUl
   }
 };
 </script>
 <style lang='less' scoped>
-.album {
-  position: absolute;
-  top: 40px;
-  width: 100%;
-  left: 0;
-  background-color: white;
-  z-index: 1000;
+.popple {
+  display: flex;
+  flex-direction: row;
+
+  .photo {
+    width: 50px;
+  }
+  .card {
+    h3 {
+      font-size: 16px;
+    }
+    .number {
+      color: gray;
+    }
+    .area {
+      color: gray;
+    }
+  }
 }
 </style>
 
