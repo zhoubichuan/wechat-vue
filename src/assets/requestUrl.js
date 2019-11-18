83e479089e2b41d1747a368131f5416b421e1255
zhoubichuan <zhoubichuan@icloud.com>
2019-11-18 23:17:22 +0800

    mock相关接口

--- src/assets/requestUrl.js
+++ src/assets/requestUrl.js
@@ -1,17 +1,28 @@
-let rect=''
-
-export default{
-    message:{
-        people:rect+'/a/b'
-
-    },
-    address:{
-
-    },
-    find:{
-
-    },
-    me:{
-
-    }
+let rect = ''
+rect = localStorage.rect ? localStorage.rect : rect
+export default {
+  message: {
+    messageList: rect + 'api/message/messageList',
+    sport: rect + 'api/message/sport',
+    talk: rect + 'api/message/talk',
+  },
+  address: {
+    newFriend: rect + 'api/address/newFriend',
+    talk: rect + 'api/address/talk',
+    tips: rect + 'api/address/tips',
+    code: rect + 'api/address/code',
+    friend: rect + 'api/address/friendList',
+    friendDetail: rect + 'api/address/friend/friendDetail'
+  },
+  find: {
+    findConfig: rect + 'api/find/findList',
+  },
+  me: {
+    selfInformation: rect + 'api/me/selfinformation',
+    setName: rect + 'api/me/information/setName',
+    QRcode: rect + 'api/me/information/QRcode',
+    moreInformation: rect + 'api/me/information/more',
+    address: rect + 'api/me/information/address',
+    payList: rect + 'api/me/pay/payList',
+  }
 }
